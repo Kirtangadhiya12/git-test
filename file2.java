40f0babff13acbd4f6f1703596d41cc6e2397444
kirtan <kirtangadhiya1212@gmail.com>
2022-01-06 15:56:03 +0530

    change the variable name

--- file2.java
+++ file2.java
@@ -11,16 +11,16 @@ public class Demo {
         System.out.print("Enter first number: ");
 
         // This method reads the number provided using keyboard
-        int num1 = scan.nextInt();
+        int num10 = scan.nextInt();
         
         System.out.print("Enter second number: ");
-        int num2 = scan.nextInt();
+        int num20 = scan.nextInt();
 
         // Closing Scanner after the use
         scan.close();
         
         // Calculating product of two numbers
-        int product = num1*num2;
+        int product = num10*num20;
         
         // Displaying the multiplication result
         System.out.println("Output: "+product);
